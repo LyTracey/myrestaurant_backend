97b04c6a9c1b2f24c6900c9032c5651196a838a5
LyTracey <lytracey@hotmail.co.uk>
2023-06-11 19:47:56 +0100

    Added URL to DATABASE settings

--- myrestaurant/settings.py
+++ myrestaurant/settings.py
@@ -94,6 +94,7 @@ WSGI_APPLICATION = 'myrestaurant.wsgi.app'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
+        'URL': os.getenv("DB_URL"),
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
