010c87e260ed725e531ccc92d60ca221d3bfe30e
LyTracey <lytracey@hotmail.co.uk>
2023-09-19 00:48:25 +0100

    Disabled localhost CORS policy

--- myrestaurant/settings.py
+++ myrestaurant/settings.py
@@ -224,7 +224,7 @@ CSRF_TRUSTED_ORIGINS = [
 
 CORS_ALLOWED_ORIGINS = [
     # "http://127.0.0.1:3000",
-    "http://localhost:3000",
+    # "http://localhost:3000",
     "https://www.tea-ly.co.uk"
 ]
 
