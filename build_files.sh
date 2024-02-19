11122295fed03cb3cda4a263136e159cc65242ac
LyTracey <lytracey@hotmail.co.uk>
2024-02-19 00:31:26 +0000

    Update wsgi application variable

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
+# Install dependencies
 pip install -r requirements.txt
+
+# Make migrations
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+
+# Collect static files 
 python3.9 manage.py collectstatic
