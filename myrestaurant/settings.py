7d0f9395ac39f321ca552955722d7455fb41ba76
LyTracey <lytracey@hotmail.co.uk>
2024-02-19 00:09:15 +0000

    Deploy to Vercel

--- myrestaurant/settings.py
+++ myrestaurant/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "tly.pythonanywhere.com"]
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 os.environ['HTTPS'] = "on"
 
 # Application definition
@@ -121,17 +121,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
-
 USE_I18N = True
 
 LANGUAGE_CODE = 'en-GB'
-
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -143,7 +135,8 @@ AUTH_USER_MODEL = 'user_app.MyUser'
 
 # For development purposes
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_files')
